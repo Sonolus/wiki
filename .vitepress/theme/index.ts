70394cd6d9ef807f9dc48c6d44c7eaff753c937b
Burrito <burritoyangster@gmail.com>
2025-03-27 19:37:28 +0800

    Changed to Sonolus themes

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { version } from '../version'
+import './index.css'
 
 export default {
     extends: DefaultTheme,
